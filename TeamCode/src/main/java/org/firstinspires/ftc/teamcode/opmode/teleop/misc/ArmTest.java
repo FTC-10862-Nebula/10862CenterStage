bbdbf2ebfedbd42078b4b3c70fadca16444fcab6
Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>
2023-11-17 18:58:35 -0600

    Servo Tests Signed-off-by: Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>
    Signed-off-by: Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/misc/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/misc/ArmTest.java
@@ -3,10 +3,11 @@ package org.firstinspires.ftc.teamcode.opmode.teleop.misc;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+
 //@Disabled
 @TeleOp
-public class ServoTest extends OpMode {
-    Servo servo1;
+public class ArmTest extends OpMode {
+    Servo servo2, servo1;
     /**
      * User defined init method
      * <p>
@@ -14,7 +15,9 @@ public class ServoTest extends OpMode {
      */
     @Override
     public void init() {
-        servo1 = hardwareMap.get(Servo.class, "armR");
+        servo2 = hardwareMap.get(Servo.class, "armR");
+        servo2.setDirection(Servo.Direction.REVERSE);
+        servo1 = hardwareMap.get(Servo.class, "armL");
         servo1.setDirection(Servo.Direction.FORWARD);
 //        servo1.setPosition(0);
     }
@@ -27,12 +30,15 @@ public class ServoTest extends OpMode {
     @Override
     public void loop() {
         if(gamepad1.dpad_down){
+            servo2.setPosition(servo2.getPosition()+ 0.001);
             servo1.setPosition(servo1.getPosition()+ 0.001);
         }
         else if(gamepad1.dpad_up){
+            servo2.setPosition(servo2.getPosition()- 0.001);
             servo1.setPosition(servo1.getPosition()- 0.001);
         }
-        
+//        servo1.setPosition(0);
+        telemetry.addData("Servo pos2: ",servo2.getPosition());
         telemetry.addData("Servo pos1: ",servo1.getPosition());
         telemetry.update();
     }

f959f0b76739a9826b0e4e173b342d55623ae5af
Nebula <droidrage.nebula@gmail.com>
2024-02-02 23:03:07 -0600

    @Disabled Roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/roadrunner/used/TurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/roadrunner/used/TurnTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmode.roadrunner.used;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.subsystems.drive.mec.MecanumDrive;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.teamcode.subsystems.drive.mec.MecanumDrive;
 /*
  * This is a simple routine to test turning capabilities.
  */
-//@Disabled
+@Disabled
 @Config
 @Autonomous(group = "drive")
 public class TurnTest extends LinearOpMode {
